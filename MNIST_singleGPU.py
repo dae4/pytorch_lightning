408a0758b101603d4d007d39bace36268eb2374f
dae4 <3210m0123@gmail.com>
2021-11-11 21:17:05 +0900

    code update

--- MNIST_singleGPU.py
+++ MNIST_singleGPU.py
@@ -56,5 +56,6 @@ val_loader = DataLoader(mnist_val, batch_size=32)
 model = LitAutoEncoder()
 
 # training
-trainer = pl.Trainer(gpus=1)
+trainer = pl.Trainer(gpus=1,max_epochs=1)
 trainer.fit(model, train_loader, val_loader)
+# %%
